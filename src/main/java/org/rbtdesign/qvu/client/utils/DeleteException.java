f9dca2740b7063b7a54bac1939173b265cf12df6
rbtucker <rbtucker16@gmail.com>
2023-09-05 10:40:40 -0500

    refactor to change path from rbt to rbtdesign

--- src/main/java/org/rbtdesign/qvu/client/utils/DeleteException.java
+++ src/main/java/org/rbtdesign/qvu/client/utils/DeleteException.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.rbt.qvu.client.utils;
+package org.rbtdesign.qvu.client.utils;
 
 /**
  *
