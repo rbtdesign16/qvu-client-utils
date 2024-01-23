f27d94ea9f62ac70c53e9a135e9099efe58c68d4
rbtucker <rbtucker16@gmail.com>
2024-01-23 17:22:42 -0600

    Revert "add filter interfaces"
    This reverts commit 802d9a7df73a48e78fc3a0a419ea234e91851a51.

--- src/main/java/org/rbtdesign/qvu/client/utils/ResponseFilter.java
+++ src/main/java/org/rbtdesign/qvu/client/utils/ResponseFilter.java
@@ -5,5 +5,5 @@ package org.rbtdesign.qvu.client.utils;
  * @author rbtuc
  */
 public interface ResponseFilter <T> {
-     public void handleResponse(T responsePayload);
+     public void handleResponse(T requestPayload);
 }
